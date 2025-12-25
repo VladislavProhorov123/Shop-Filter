0e582d4e322aa25ccd5e13c5756afb4954dd1837
Vladislav <vladislavprokhorov1005@gmail.com>
2025-12-25 18:08:53 +0200

    chore: add base project architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Nav from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from './Recommended/Recommended'
 
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Nav />
+      <Products />
+      <Recommended />
     </div>
   )
 }
